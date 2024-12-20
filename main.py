6aa88c5f0a013e6ab99ccb9e677731cc2cfaceb4
ShahriarAlam47 <johnshah2022@gmail.com>
2024-12-20 22:31:27 +0600

    sixth commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Item:
-    pay_rate = 0.8  # Class attribute
+    pay_rate = 0.8  # Class attribute: Added value after 20% discount
 
     def __init__(self, name: str, price: float, quantity=0):   # Magic method
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
@@ -11,12 +11,20 @@ class Item:
     def calculate_total_cost(self):
         return self.price * self.quantity
     def apply_discount(self):
-        self.price = self.price * Item.pay_rate
+        self.price = self.price * self.pay_rate
 
 
-item1 = Item("Phone", 1000, 1)
+item1 = Item("Phone", 100, 1)
 item1.apply_discount()
 print(item1.price)
+
+item2 = Item("Laptop", 1000, 3)
+item2.pay_rate = 0.7  # Added value after 30% discount
+item2.apply_discount()
+print(item2.price)
+print(item2.calculate_total_cost())  # Printed price of three laptop on 30% discount
+
+
 
 
 
