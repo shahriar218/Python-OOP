d99bf011115b0295db6ab72f4017ede4fa1996e4
ShahriarAlam47 <johnshah2022@gmail.com>
2024-12-20 21:43:52 +0600

    fourth commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Item:
     pay_rate = 0.8  # Class attribute
 
-    def __init__(self, name: str, price: float, quantity=0):
+    def __init__(self, name: str, price: float, quantity=0):   # Magic method
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
         self.name = name
@@ -15,8 +15,8 @@ class Item:
 item1 = Item("Phone", 1000, 1)
 item2 = Item("Laptop", 5000, 3)
 
-print(Item.pay_rate)
-print(item1.pay_rate)
-print(item2.pay_rate)
+print(Item.__dict__)  # Magic attribute: All the attribute for Class level
+print(item1.__dict__)  # Magic attribute: All the attribute for Class level
+
 
 
